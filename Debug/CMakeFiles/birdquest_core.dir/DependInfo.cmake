b7fe6b20706a9d2500a4e41f841edee9af66a7ab
Sejin Park <snpark01@email.wm.edu>
2019-10-21 01:40:27 -0400

    Fixed battle menu

---
+++ Debug/CMakeFiles/birdquest_core.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/BBBorder.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BBBorder.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/BattleBar.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BattleBar.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/BattleMenu.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BattleMenu.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/Indicator.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/Indicator.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/LevelManager.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/LevelManager.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/TileMap.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/TileMap.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/battle.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/battle.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/battle_sys_Belcher.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/battle_sys_Belcher.cpp.obj"
+  "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/src/tinyxml2.cpp" "C:/Users/Jin Park/Desktop/Bird Quest/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/tinyxml2.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/SFML-2.4.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

9cf4c8b9479efce196eeaf735d50629313263ab5
Sejin Park <snpark01@email.wm.edu>
2019-10-22 00:51:36 -0400

    Fixed issues with battle bar damage output

---
+++ Debug/CMakeFiles/birdquest_core.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libbirdquest_core.a"
+)
---
+++ Debug/CMakeFiles/xmltest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xmltest.dir/bin/xmltest.cpp.obj"
+  "libxmltest.dll.a"
+  "xmltest.exe"
+  "xmltest.exe.manifest"
+  "xmltest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xmltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

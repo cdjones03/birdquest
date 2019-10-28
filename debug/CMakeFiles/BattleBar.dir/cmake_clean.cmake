e73a72c52eb2747a7f00808d8afc7fe89b39b8f3
Sydnee Belcher <snbelcher@email.wm.edu>
2019-10-28 01:51:39 -0400

    created the map for outside of the dungeon, made some tile sets transparent

---
+++ debug/CMakeFiles/BattleBar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BattleBar.exe"
+  "BattleBar.exe.manifest"
+  "BattleBar.pdb"
+  "CMakeFiles/BattleBar.dir/bin/BattleBar.cpp.obj"
+  "libBattleBar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BattleBar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ debug/CMakeFiles/birdquest_core.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/BBBorder.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/BBBorder.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/BattleBar.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/BattleBar.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/BattleLogic.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/BattleLogic.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/BattleMenu.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/BattleMenu.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/Indicator.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/Indicator.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/LevelManager.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/LevelManager.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/TileMap.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/TileMap.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/battle.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/battle.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/battle_sys_Belcher.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/battle_sys_Belcher.cpp.obj"
+  "C:/CSCI437_GameDesign/Game_Project/birdquest/src/tinyxml2.cpp" "C:/CSCI437_GameDesign/Game_Project/birdquest/debug/CMakeFiles/birdquest_core.dir/src/tinyxml2.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/SFML/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

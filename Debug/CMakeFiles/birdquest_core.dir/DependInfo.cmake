fbad8c121559cbc3a964c3f3e4c5261267fa7423
Sejin Park <snpark01@email.wm.edu>
2019-12-03 00:24:59 -0500

    added sound effect to attack in battle menu

---
+++ Debug/CMakeFiles/birdquest_core.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Jin Park/Desktop/birdquest/src/BBBorder.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BBBorder.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/BattleBar.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BattleBar.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/BattleLogic.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BattleLogic.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/BattleMenu.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/BattleMenu.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/EndScreen.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/EndScreen.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/Enemy.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/Enemy.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/EnemyLogic.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/EnemyLogic.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/HumanView.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/HumanView.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/Indicator.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/Indicator.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/Inventory.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/Inventory.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/LevelLogic.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/LevelLogic.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/LevelManager.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/LevelManager.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/PauseMenu.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/PauseMenu.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/Screen.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/Screen.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/TileMap.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/TileMap.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/TitleScreen.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/TitleScreen.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/battle.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/battle.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/battle_sys_Belcher.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/battle_sys_Belcher.cpp.obj"
+  "C:/Users/Jin Park/Desktop/birdquest/src/tinyxml2.cpp" "C:/Users/Jin Park/Desktop/birdquest/Debug/CMakeFiles/birdquest_core.dir/src/tinyxml2.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/SFML-2.4.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
